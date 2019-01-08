c691ad6fba108020b27918f3d7384e57fbe05062
M4xw <m4x@m4xw.net>
2019-01-09 00:14:24 +0100

    [LIBNX] Dont default to dynamic_recompiler

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -132,7 +132,11 @@ static void setup_variables(void)
     struct retro_variable variables[] = {
         { "mupen64plus-cpucore",
 #ifdef DYNAREC
+#ifdef HAVE_LIBNX
+            "CPU Core; cached_interpreter|pure_interpreter|dynamic_recompiler" },
+#else
             "CPU Core; dynamic_recompiler|cached_interpreter|pure_interpreter" },
+#endif
 #else
             "CPU Core; cached_interpreter|pure_interpreter" },
 #endif
